8a4c2a43206e03e7335737660d674eada5f0a1ad
Janis Ganga <janisganga@MacBook-Pro-de-Janis.local>
2023-02-19 17:44:55 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+# setup(
+#     name='janetic',
+#     version='0.1.0',
+#     author='Janis Ganga',
+#     author_email='janisganga@gmail.com',
+#     description='A Python library for genetic algorithms',
+#     packages=find_packages(),
+#     install_requires=[
+#         'numpy>=1.18.1',
+#         'scipy>=1.4.1',
+#     ],
+#     entry_points={
+#         'console_scripts': [
+#             'janetic=janetic.cli:janetic',
+#         ],
+#     },
+# )
+
+setup(
+    name='janetic',
+    version='0.1.0',
+    description='A Python library for genetic algorithms',
+    author='Janis Ganga',
+    packages=find_packages(),
+    install_requires=[],
+)
